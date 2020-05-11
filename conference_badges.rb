5a5e82b61d764dd7d826f71c8de0b2f426f3a436
Victor Williams <victor.williams1030@gmail.com>
2020-05-11 19:51:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,6 @@ def batch_badge_creator(attendees)
   badge_message = []
   attendees.each do |name|
     badge_message.push(badge_maker(name))
-  return badge_message
+    return badge_message
   end
 end
